1488c52d0aa0c80d135cec2267a90c47bf8a31c1
lean1508 <leacaorsi@hotmail.com>
2020-07-04 01:09:21 -0300

    Agregue los html de las categorias

--- site/src/controllers/productoController.js
+++ site/src/controllers/productoController.js
@@ -2,7 +2,23 @@ const path = require('path');
 
 module.exports = {
     show: (req,res)=>{res.sendFile(path.resolve(__dirname, '..', 'views', 'producto', 'productDetail.html'))
-},
+    },
     verTintos: (req,res)=>{res.sendFile(path.resolve(__dirname, '..', 'views', 'producto', 'vinosTintos.html'))
-}
+    },
+    verBlancos: (req,res)=>{res.sendFile(path.resolve(__dirname, '..', 'views', 'producto', 'vinosBlancos.html'))
+    },
+    verRosados: (req,res)=>{res.sendFile(path.resolve(__dirname, '..', 'views', 'producto', 'vinosRosados.html'))
+    },
+    verEspumantes: (req,res)=>{res.sendFile(path.resolve(__dirname, '..', 'views', 'producto', 'vinosEspumantes.html'))
+    },
+    verEspirituosos: (req,res)=>{res.sendFile(path.resolve(__dirname, '..', 'views', 'producto', 'espirituosos.html'))
+    },
+    verParaBrindar: (req,res)=>{res.sendFile(path.resolve(__dirname, '..', 'views', 'producto', 'paraBrindar.html'))
+    },
+    verMasVendidos: (req,res)=>{res.sendFile(path.resolve(__dirname, '..', 'views', 'producto', 'masVendidos.html'))
+    },
+    verRecomendados: (req,res)=>{res.sendFile(path.resolve(__dirname, '..', 'views', 'producto', 'recomendados.html'))
+    },
+    verPromos: (req,res)=>{res.sendFile(path.resolve(__dirname, '..', 'views', 'producto', 'promos.html'))
+    }
 }
--- site/src/routes/producto.js
+++ site/src/routes/producto.js
@@ -6,5 +6,13 @@ const productoController = require(path.resolve(__dirname, '..', 'controllers', 
 
 router.get('/producto/detalle', productoController.show);
 router.get('/vinosTintos', productoController.verTintos);
+router.get('/vinosBlancos', productoController.verBlancos);
+router.get('/vinosRosados', productoController.verRosados);
+router.get('/vinosEspumantes', productoController.verEspumantes);
+router.get('/espirituosos', productoController.verEspirituosos);
+router.get('/paraBrindar', productoController.verParaBrindar);
+router.get('/masVendidos', productoController.verMasVendidos);
+router.get('/recomendados', productoController.verRecomendados);
+router.get('/promos', productoController.verPromos);
 
 module.exports = router;
