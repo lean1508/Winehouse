f4a08408e30d05fcf8ce14c43eb3b9917f8501eb
lean1508 <leacaorsi@hotmail.com>
2020-07-23 18:35:02 -0300

    Se agrega la funcionalidad al Registro

--- site/src/routes/user.js
+++ site/src/routes/user.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const multer = require('multer');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '..','..','public','images','usuarios'));
+    },
+    filename: function (req, file, cb) {
+      cb(null, 'usuario-'+Date.now() + path.extname(file.originalname));
+    }
+  })
+   
+const upload = multer({ storage })
 
 const userController = require(path.resolve(__dirname, '..', 'controllers', 'userController'));
 
 router.get('/register', userController.index);
+router.post('/register', upload.single('avatar'), userController.register);
 
 module.exports = router;
